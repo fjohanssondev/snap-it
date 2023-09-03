ef575c620115c414b98e287563c2238344ce610d
fjohanssonnn <fjohansson.creative@gmail.com>
2023-09-03 18:57:08 +0200

    Removed the header from the snap-page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <Tabs>
       <Tabs.Screen name="Home" />
-      <Tabs.Screen name="Snap" />
+      <Tabs.Screen name="Snap" options={{ headerShown: false }} />
       <Tabs.Screen name="test" />
     </Tabs>
   )
