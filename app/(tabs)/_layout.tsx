f6e811b5cb15f5735aa75857c4bfca28c30fcd21
fjohanssonnn <fjohansson.creative@gmail.com>
2023-09-04 13:23:38 +0200

    Remove header from Home-screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "expo-router";
 export default () => {
   return (
     <Tabs>
-      <Tabs.Screen name="Home" />
+      <Tabs.Screen name="Home" options={{ headerShown: false }} />
       <Tabs.Screen name="Snap" options={{ headerShown: false }} />
       <Tabs.Screen name="test" />
     </Tabs>
